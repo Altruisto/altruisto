c98e896dbfd1990bb66dd73913acfdfe8ab1842c
kovsky0 <wyrzykowski.daniel@gmail.com>
2019-07-13 10:51:26 +0200

    build: add prettier and format existing code

--- src/content/topbar.js
+++ src/content/topbar.js
@@ -1,15 +1,15 @@
-import { ASSETS_PATHS } from '../helpers/assets-paths.js';
-import { extractDomain } from '../helpers/extract-domain.js';
+import { ASSETS_PATHS } from "../helpers/assets-paths.js"
+import { extractDomain } from "../helpers/extract-domain.js"
 
-const DOMAIN = extractDomain(location.href);
+const DOMAIN = extractDomain(location.href)
 
 /**
  * Change topbar's inner HTML.
  */
 function updateTopbar(activated) {
-    let innerHTML = getInnerHTML(activated);
-    let topbarElement = document.getElementById('Altruisto');
-    topbarElement.innerHTML = innerHTML;
+  let innerHTML = getInnerHTML(activated)
+  let topbarElement = document.getElementById("Altruisto")
+  topbarElement.innerHTML = innerHTML
 }
 
 /**
@@ -20,29 +20,29 @@ function updateTopbar(activated) {
  */
 // eslint-disable-next-line no-unused-vars
 function deactivateAffiliate() {
-    chrome.runtime.sendMessage({domain: DOMAIN}, function(response) {
-        if(response.status === true){
-            updateTopbar(false);
-        }
-    });
+  chrome.runtime.sendMessage({ domain: DOMAIN }, function(response) {
+    if (response.status === true) {
+      updateTopbar(false)
+    }
+  })
 }
 
 /**
  * Set topbar's display property to none and save to storage.local the information that on this website the topbar should not be (temporarily) displayed.
  */
-function hideTopbar(){
-    let updatedClosedWebsites = [];
+function hideTopbar() {
+  let updatedClosedWebsites = []
 
-    document.getElementById("AltruistoTopBar").style.display = 'none';
-    //moveWebsite('-50px');
+  document.getElementById("AltruistoTopBar").style.display = "none"
+  //moveWebsite('-50px');
 
-    chrome.storage.local.get({closedWebsites: []}, function(items) {
-        if(items.closedWebsites.indexOf(DOMAIN) == -1){
-            updatedClosedWebsites = items.closedWebsites;
-            updatedClosedWebsites.push(DOMAIN);
-            chrome.storage.local.set({'closedWebsites': updatedClosedWebsites});
-        }
-    });
+  chrome.storage.local.get({ closedWebsites: [] }, function(items) {
+    if (items.closedWebsites.indexOf(DOMAIN) == -1) {
+      updatedClosedWebsites = items.closedWebsites
+      updatedClosedWebsites.push(DOMAIN)
+      chrome.storage.local.set({ closedWebsites: updatedClosedWebsites })
+    }
+  })
 }
 
 /**
@@ -50,10 +50,12 @@ function hideTopbar(){
  *
  * @param {boolean} activated Topbar's activation status.
  */
-function addListeners(activated){
-    document.getElementById("AltruistoTopBarIcon").addEventListener("click", function(){
-        hideTopbar();
-    });
+function addListeners(activated) {
+  document
+    .getElementById("AltruistoTopBarIcon")
+    .addEventListener("click", function() {
+      hideTopbar()
+    })
 }
 
 /**
@@ -61,20 +63,38 @@ function addListeners(activated){
  *
  * @param {boolean} activated Topbar's activation status.
  */
-function getContent(activated){
-    let content;
+function getContent(activated) {
+  let content
 
-    if(activated){
-        content = chrome.i18n.getMessage('topbarActivatedInfo') + '<p id="AltruistoSmallText">' + chrome.i18n.getMessage('topbarActivatedClose') + '</p>';
-    }
-    else if(DOMAIN.indexOf('ebay') !== -1) {
-        content = chrome.i18n.getMessage('topbarActivateInfo') + '<a href=https://altruisto.com/confirm?url=' + location.href + '&lang=' + chrome.i18n.getUILanguage() + ' id=AltruistoTopBarButton>' + chrome.i18n.getMessage('topbarActivateButton') + '</a>';
-    }
-    else {
-        content = chrome.i18n.getMessage('topbarActivateInfo') + '<a href=https://altruisto.com/redirect?url=' + location.href + '&lang=' + chrome.i18n.getUILanguage() + ' id=AltruistoTopBarButton>' + chrome.i18n.getMessage('topbarActivateButton') + '</a>';
-    }
+  if (activated) {
+    content =
+      chrome.i18n.getMessage("topbarActivatedInfo") +
+      '<p id="AltruistoSmallText">' +
+      chrome.i18n.getMessage("topbarActivatedClose") +
+      "</p>"
+  } else if (DOMAIN.indexOf("ebay") !== -1) {
+    content =
+      chrome.i18n.getMessage("topbarActivateInfo") +
+      "<a href=https://altruisto.com/confirm?url=" +
+      location.href +
+      "&lang=" +
+      chrome.i18n.getUILanguage() +
+      " id=AltruistoTopBarButton>" +
+      chrome.i18n.getMessage("topbarActivateButton") +
+      "</a>"
+  } else {
+    content =
+      chrome.i18n.getMessage("topbarActivateInfo") +
+      "<a href=https://altruisto.com/redirect?url=" +
+      location.href +
+      "&lang=" +
+      chrome.i18n.getUILanguage() +
+      " id=AltruistoTopBarButton>" +
+      chrome.i18n.getMessage("topbarActivateButton") +
+      "</a>"
+  }
 
-    return content;
+  return content
 }
 
 /**
@@ -83,14 +103,14 @@ function getContent(activated){
  * @param {boolean} activated Topbar's activation status.
  */
 function getInnerHTML(activated) {
-    let template = require('./topbar.html');
-    let content = getContent(activated);
-    let innerHTML = template({
-        ASSETS_PATHS: ASSETS_PATHS,
-        content: content,
-    });
+  let template = require("./topbar.html")
+  let content = getContent(activated)
+  let innerHTML = template({
+    ASSETS_PATHS: ASSETS_PATHS,
+    content: content
+  })
 
-    return innerHTML;
+  return innerHTML
 }
 
 /**
@@ -99,114 +119,118 @@ function getInnerHTML(activated) {
  * @param {boolean} activated Topbar's activation status.
  */
 function renderTopbar(activated) {
-    let style = require('./topbar.css').toString();
-    let styleElement = document.createElement('style');
-    styleElement.innerHTML = style;
+  let style = require("./topbar.css").toString()
+  let styleElement = document.createElement("style")
+  styleElement.innerHTML = style
 
-    let innerHTML = getInnerHTML(activated);
+  let innerHTML = getInnerHTML(activated)
 
-    let topbarElement = document.createElement('div');
-    topbarElement.id = 'Altruisto';
-    //arabic should be displayed from the right to the left
-    if (chrome.i18n.getUILanguage() == 'ar') {
-        topbarElement.dir = 'rtl';
-    }
-    topbarElement.innerHTML = innerHTML;
+  let topbarElement = document.createElement("div")
+  topbarElement.id = "Altruisto"
+  //arabic should be displayed from the right to the left
+  if (chrome.i18n.getUILanguage() == "ar") {
+    topbarElement.dir = "rtl"
+  }
+  topbarElement.innerHTML = innerHTML
 
-    //moveWebsite('50px');
+  //moveWebsite('50px');
 
-    document.documentElement.prepend(styleElement);
-    document.documentElement.prepend(topbarElement);
+  document.documentElement.prepend(styleElement)
+  document.documentElement.prepend(topbarElement)
 
-    if(activated){
-        setInterval(function(){
-            hideTopbar();
-        }, 6000);
-    }
+  if (activated) {
+    setInterval(function() {
+      hideTopbar()
+    }, 6000)
+  }
 
-    addListeners(activated);
+  addListeners(activated)
 }
-
 
 /**
  * Check if raising money with current website has been already activated (and not longer than 7 days ago).
  *
  * @param {array} activatedAffiliates Array containing domains and timestamps of affiliates that has already been activated. Should be loaded from storage.local.
  */
-function isAlreadyActivated(activatedAffiliates){
-    //if activatedAffiliates has any values
-    if(activatedAffiliates.length){
-        //loop through all the values
-        for(let i = 0; i < activatedAffiliates.length; i++){
-            //if current page is in our list of already activated pages
-            if(activatedAffiliates[i].domain == DOMAIN){
-                //if page was activated not longer than 7 days ago
-                let activationEndTimestamp = activatedAffiliates[i].timestamp + 1000*60*60*24*7;
-                let currentTimestamp = Date.now();
-                if(activationEndTimestamp > currentTimestamp ){
-                    return true;
-                }
-            }
+function isAlreadyActivated(activatedAffiliates) {
+  //if activatedAffiliates has any values
+  if (activatedAffiliates.length) {
+    //loop through all the values
+    for (let i = 0; i < activatedAffiliates.length; i++) {
+      //if current page is in our list of already activated pages
+      if (activatedAffiliates[i].domain == DOMAIN) {
+        //if page was activated not longer than 7 days ago
+        let activationEndTimestamp =
+          activatedAffiliates[i].timestamp + 1000 * 60 * 60 * 24 * 7
+        let currentTimestamp = Date.now()
+        if (activationEndTimestamp > currentTimestamp) {
+          return true
         }
+      }
     }
+  }
 }
 
 /**
  * Check if the user is on a checkout page of one of our biggest partners.
  */
 function isCheckoutPage() {
-    if(DOMAIN == 'booking.com' && location.href.indexOf('book.html') !== -1) {
-        return true;
-    }
-    else if(DOMAIN == 'etsy.com' && location.href.indexOf('/cart/') !== -1) {
-        return true;
-    }
-    else if(DOMAIN == 'aliexpress.com' && location.href.indexOf('/confirm_order.htm') !== -1) {
-        return true;
-    }
-    else if(DOMAIN == 'barnesandnoble.com' && location.href.indexOf('/checkout/') !== -1) {
-        return true;
-    }
-    else {
-        return false;
-    }
+  if (DOMAIN == "booking.com" && location.href.indexOf("book.html") !== -1) {
+    return true
+  } else if (DOMAIN == "etsy.com" && location.href.indexOf("/cart/") !== -1) {
+    return true
+  } else if (
+    DOMAIN == "aliexpress.com" &&
+    location.href.indexOf("/confirm_order.htm") !== -1
+  ) {
+    return true
+  } else if (
+    DOMAIN == "barnesandnoble.com" &&
+    location.href.indexOf("/checkout/") !== -1
+  ) {
+    return true
+  } else {
+    return false
+  }
 }
 
-export default function () {
-    chrome.storage.local.get({
-            activatedAffiliates: [],
-            closedWebsites: [],
-            disabledWebsites: [],
-            partners: []
-        }, function(items) {
+export default function() {
+  chrome.storage.local.get(
+    {
+      activatedAffiliates: [],
+      closedWebsites: [],
+      disabledWebsites: [],
+      partners: []
+    },
+    function(items) {
+      //if current domain is one of our partners
+      if (items.partners.indexOf(DOMAIN) != -1) {
+        //if current domain is not on disabled or closed websites list
+        if (
+          items.closedWebsites.indexOf(DOMAIN) == -1 &&
+          items.disabledWebsites.indexOf(DOMAIN) == -1
+        ) {
+          let activated = false
+          if (isAlreadyActivated(items.activatedAffiliates)) {
+            activated = true
+          }
 
-            //if current domain is one of our partners
-            if(items.partners.indexOf(DOMAIN) != -1){
+          renderTopbar(activated)
+        }
 
-                //if current domain is not on disabled or closed websites list
-                if(items.closedWebsites.indexOf(DOMAIN) == -1
-                && items.disabledWebsites.indexOf(DOMAIN) == -1){
-                    let activated = false;
-                    if(isAlreadyActivated(items.activatedAffiliates)){
-                        activated = true;
-                    }
+        //if user is on checkout page - show
+        if (
+          items.disabledWebsites.indexOf(DOMAIN) == -1 &&
+          isCheckoutPage(location.href)
+        ) {
+          let activated = false
+          if (isAlreadyActivated(items.activatedAffiliates)) {
+            activated = true
+          }
 
-                    renderTopbar(activated);
-                }
-
-                //if user is on checkout page - show
-                if(items.disabledWebsites.indexOf(DOMAIN) == -1
-                && isCheckoutPage(location.href)){
-                    let activated = false;
-                    if(isAlreadyActivated(items.activatedAffiliates)){
-                        activated = true;
-                    }
-
-                    renderTopbar(activated);
-                }
-
-            }
-
-        });
-
+          renderTopbar(activated)
+        }
+      }
+    }
+  )
 }
--- src/pages/options.js
+++ src/pages/options.js
@@ -1,17 +1,17 @@
 function replace_i18n(obj, tag) {
   var msg = tag.replace(/__MSG_(\w+)__/g, function(match, v1) {
-      return v1 ? chrome.i18n.getMessage(v1) : '';
-  });
+    return v1 ? chrome.i18n.getMessage(v1) : ""
+  })
 
-  if(msg != tag) {
-    obj.innerHTML = msg;
+  if (msg != tag) {
+    obj.innerHTML = msg
     //arabic should be displayed from the right to the left
-    if(chrome.i18n.getUILanguage() == 'ar') {
-      obj.dir = 'rtl';
+    if (chrome.i18n.getUILanguage() == "ar") {
+      obj.dir = "rtl"
       //fix sliders position
-      var sliders = document.getElementsByClassName('col-25');
-      for(var i = 0; i < sliders.length; i++) {
-        sliders[i].style.textAlign = 'left';
+      var sliders = document.getElementsByClassName("col-25")
+      for (var i = 0; i < sliders.length; i++) {
+        sliders[i].style.textAlign = "left"
       }
     }
   }
@@ -19,51 +19,57 @@ function replace_i18n(obj, tag) {
 
 function localizeHtmlPage() {
   // Localize everything else by replacing all __MSG_***__ tags
-  var page = document.getElementsByTagName('html');
-  
-  for (var j = 0; j < page.length; j++) {
-      var obj = page[j];
-      var tag = obj.innerHTML.toString();
+  var page = document.getElementsByTagName("html")
 
-      replace_i18n(obj, tag);
+  for (var j = 0; j < page.length; j++) {
+    var obj = page[j]
+    var tag = obj.innerHTML.toString()
+
+    replace_i18n(obj, tag)
   }
 }
 
 function save_options() {
-  var addSuggestionBox = document.getElementById('add_suggestion_box').checked;
-  var addTopBar = document.getElementById('add_top_bar').checked;
-  
-  chrome.storage.sync.set({
-    addSuggestionBox: addSuggestionBox,
-    addTopBar: addTopBar
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options updated.';
-    status.style.display = 'block';
-    setTimeout(function() {
-      status.style.display = 'none';
-    }, 750);
-  });
+  var addSuggestionBox = document.getElementById("add_suggestion_box").checked
+  var addTopBar = document.getElementById("add_top_bar").checked
+
+  chrome.storage.sync.set(
+    {
+      addSuggestionBox: addSuggestionBox,
+      addTopBar: addTopBar
+    },
+    function() {
+      // Update status to let user know options were saved.
+      var status = document.getElementById("status")
+      status.textContent = "Options updated."
+      status.style.display = "block"
+      setTimeout(function() {
+        status.style.display = "none"
+      }, 750)
+    }
+  )
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  chrome.storage.sync.get({
-    addSuggestionBox: false,
-    addTopBar: true
-  }, function(items) {
-    document.getElementById('add_suggestion_box').checked = items.addSuggestionBox;
-    document.getElementById('add_top_bar').checked = items.addTopBar;
-  });
+  chrome.storage.sync.get(
+    {
+      addSuggestionBox: false,
+      addTopBar: true
+    },
+    function(items) {
+      document.getElementById("add_suggestion_box").checked =
+        items.addSuggestionBox
+      document.getElementById("add_top_bar").checked = items.addTopBar
+    }
+  )
 }
 
-localizeHtmlPage();
+localizeHtmlPage()
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('add_suggestion_box').addEventListener('click',
-    save_options);
-document.getElementById('add_top_bar').addEventListener('click',
-    save_options);
-
+document.addEventListener("DOMContentLoaded", restore_options)
+document
+  .getElementById("add_suggestion_box")
+  .addEventListener("click", save_options)
+document.getElementById("add_top_bar").addEventListener("click", save_options)
