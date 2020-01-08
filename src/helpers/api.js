a26f0d2477629a05652131c92e1c6728cfd029af
Daniel Wyrzykowski <daniel.wyrzykowski@pionerlabs.com>
2020-01-08 11:31:03 +0100

    refactor: get api url from single place

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const BASE_URL = "http://api.altruisto.localhost:8000"
+export const BASE_URL = "http://api.altruisto.localhost:8001"
 
 export default axios.create({
   baseURL: BASE_URL
