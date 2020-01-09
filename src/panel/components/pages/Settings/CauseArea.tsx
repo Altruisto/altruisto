e083212fab47978d632608b4cb10316bf7a139b1
Daniel Wyrzykowski <daniel.wyrzykowski@pionerlabs.com>
2020-01-09 12:27:40 +0100

    fix: 'Animals Suffering Reduction' -> 'Reduction of Animal Suffering'

--- src/panel/components/pages/Settings/CauseArea.tsx
+++ src/panel/components/pages/Settings/CauseArea.tsx
@@ -10,7 +10,7 @@ export const CauseArea = props => {
         <MenuItem value="extreme_poverty">
           Extreme Poverty &amp; Global Health
         </MenuItem>
-        <MenuItem value="animals">Animals Suffering Reduction</MenuItem>
+        <MenuItem value="animals">Reduction of Animal Suffering</MenuItem>
       </Select>
     </div>
   )
