6094f8dca5f935374219c38db0f411058e84d0db
Daniel Wyrzykowski <kovsky0@users.noreply.github.com>
2020-03-21 16:42:20 +0100

    fix: twitter share button on welcome page

--- website/pages/welcome.tsx
+++ website/pages/welcome.tsx
@@ -16,7 +16,7 @@ const Welcome = () => (
             className="button mr-3"
             id="share-button-facebook"
             target="_blank"
-            href="https://www.facebook.com/sharer/sharer.php?u=https%3A//altruisto.com?ref=facebook_share"
+            href="https://www.facebook.com/sharer/sharer.php?u=https%3A//altruisto.com?ref=welcome_facebook_share"
             style={{ background: "#4267b2!important", borderColor: "#4267b2", minWidth: "200px" }}
           >
             Share on Facebook
@@ -25,7 +25,7 @@ const Welcome = () => (
             className="button"
             id="share-button-twitter"
             target="_blank"
-            href="https://twitter.com/home?status=Install%20an%20extension%20and%20when%20you%20buy%20stuff%20online,%20people%20in%20extreme%20poverty%20will%20get%20medicines,%20bed nets,%20or%20money%20https://altruisto.com?utm_source=twittter_share"
+            href="https://twitter.com/intent/tweet?url=https%3A%2F%2Faltruisto.com%2F%3Fref%3Dwelcome_twitter_share&text=Install%20an%20extension%20and%20when%20you%20buy%20stuff%20online%2C%20people%20in%20extreme%20poverty%20will%20get%20medicines%2C%20bed%20nets%2C%20or%20money%20"
             style={{ background: "#1da1f2!important", borderColor: "#1da1f2", minWidth: "200px" }}
           >
             Share on Twitter
