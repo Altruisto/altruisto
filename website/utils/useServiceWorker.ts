2517019180ec53afbd0e2bb9b28e7ddc4a402e15
Artur Kozubek <artur.kozubek1@gmail.com>
2020-04-23 11:34:24 +0200

    format

--- website/utils/useServiceWorker.ts
+++ website/utils/useServiceWorker.ts
@@ -1,9 +1,9 @@
 import { useEffect } from "react"
 
 export const useServiceWorker = () => {
-    useEffect(() => {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('sw.js');
-        }
-    },[])
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("sw.js")
+    }
+  }, [])
 }
