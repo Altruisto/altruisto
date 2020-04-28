6213e385dde29ec8bb7c44fd1e6ad037942fca84
Daniel Wyrzykowski <kovsky0@users.noreply.github.com>
2020-04-28 07:58:25 +0200

    fix: modified search results should not have links (for ebay)

--- extension/src/search_results/google.ts
+++ extension/src/search_results/google.ts
@@ -1,23 +1,22 @@
 import { extractDomain } from "../helpers/extract-domain"
 import { ASSETS_PATHS } from "../helpers/assets-paths"
-import { getTracker } from "../helpers/get-tracker"
 import { storage } from "../helpers/storage"
 
 const SEARCH_RESULT_QUERY = ".g"
 const RESULT_URL_QUERY = "a:first-of-type"
 
-const highlightSearchResult = (result: Element, url: string, tracker: string) => {
+const highlightSearchResult = (result: Element, url: string) => {
   const highlight = document.createElement("div")
   // TODO: fix scaling image with css
   highlight.innerHTML = `
-    <a href="${BASE_URL}/redirect/?url=${url}&tracker=${tracker}" target="_blank" rel="noreferrer noopener" style="color: #e70f74">
+    <span style="color: #e70f74">
       <img src="${ASSETS_PATHS.icons.icon48}" alt="Altruisto logo" title="Altruisto logo" style="margin-left: -34px; max-width: 32px"/>
       <span style="background-image: linear-gradient(136deg, #e70f74, #ff2525);
       color: transparent;
       -webkit-background-clip: text;
       background-clip: text;
-      font-weight: bold;"><strong>Click here</strong> to giveaway portion of your purchases to charities (for free!)</span>
-    </a>
+      font-weight: bold;">This site is altruisto's partner. You can donate portion of your purchases to charities!</span>
+    </span>
   `
   result.prepend(highlight)
 }
@@ -30,7 +29,7 @@ storage.get("sync", "highlightSearchResults").then(({ highlightSearchResults }) 
         const url = result.querySelector<HTMLAnchorElement>(RESULT_URL_QUERY)!.href
         const domain = extractDomain(url)
         if (partners.includes(domain)) {
-          getTracker.then(tracker => highlightSearchResult(result, url, tracker))
+          highlightSearchResult(result, url)
         }
       })
     })
