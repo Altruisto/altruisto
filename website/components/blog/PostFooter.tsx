090189807bb22aa18ed8c65c3457cec38ca8702f
mgonciarz <mikolaj.gonciarz@dreamlab.pl>
2020-08-17 23:35:48 +0200

    added author component

--- website/components/blog/PostFooter.tsx
+++ website/components/blog/PostFooter.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useRouter } from "next/router"
 
 import { FacebookShareButton, TwitterShareButton } from "react-share"
+import Author from "./Author"
 
 const PostFooter: React.FC = () => {
   const { asPath } = useRouter()
@@ -9,13 +10,16 @@ const PostFooter: React.FC = () => {
 
   return (
     <div className="blog__post-footer">
-      <div className="my-4 d-flex justify-content-center">
-        <TwitterShareButton className="mx-3" url={shareUrl}>
-          <img src="/images/tw@2x.svg" width="16" height="16" alt="Facebook share" />
-        </TwitterShareButton>
-        <FacebookShareButton className="mx-2" url={shareUrl}>
-          <img src="/images/fb@2x.svg" width="16" height="16" alt="Facebook share" />
-        </FacebookShareButton>
+      <div className="my-4 d-flex justify-content-between">
+        <Author name="altruisto" />
+        <div className="d-flex ml-md-auto mr-md-auto justify-content-center">
+          <TwitterShareButton className="mx-3" url={shareUrl}>
+            <img src="/images/tw@2x.svg" width="16" height="16" alt="Facebook share" />
+          </TwitterShareButton>
+          <FacebookShareButton className="mx-2" url={shareUrl}>
+            <img src="/images/fb@2x.svg" width="16" height="16" alt="Facebook share" />
+          </FacebookShareButton>
+        </div>
       </div>
       <hr />
     </div>
