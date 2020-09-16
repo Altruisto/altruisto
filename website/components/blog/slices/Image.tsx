afb80c2562669e6ac99ff7c76e5a01f769edc964
Daniel Wyrzykowski <kovsky0@users.noreply.github.com>
2020-09-16 09:53:15 +0200

    fix: aligning images

--- website/components/blog/slices/Image.tsx
+++ website/components/blog/slices/Image.tsx
@@ -2,21 +2,26 @@ import { useMemo } from "react"
 
 export default ({ slice }) => {
   const { aligment, image, title1 } = slice.primary
+  const text = aligment[0].text || ""
   const imageClassName = useMemo(() => {
-    switch (aligment[0].text) {
+    switch (text || "center") {
       case "left":
-        return "blog__post-image__left"
+        return "blog__post-image--left"
       case "right":
-        return "blog__post-image__right"
+        return "blog__post-image--right"
+      case "full-width":
+        return "blog__post-image--full-width"
       default:
-        return "blog__post-image__center"
+        return "blog__post-image--center"
     }
-  }, [aligment[0].text])
+  }, [text])
 
   return (
     <figure className={`blog__post-image ${imageClassName}`}>
       <img className="blog--rounded mb-2" src={image.url} alt={image.alt} />
-      {title1[0] && title1[0].text && <figcaption>{title1[0].text}</figcaption>}
+      {title1[0] && title1[0].text && (
+        <figcaption className="blog__post-image-caption">{title1[0].text}</figcaption>
+      )}
     </figure>
   )
 }
