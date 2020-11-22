2f571a2bbc433d9013bece545b04e361b010dc5b
Lars Mennen <mennen.lars@gmail.com>
2020-11-22 23:12:51 +0100

    Finish up page and make it responsive

--- website/custom/index/assets/js/progress.js
+++ website/custom/index/assets/js/progress.js
@@ -2,12 +2,37 @@ import './jquery.svg.min';
 import './jquery.svganim.min';
 
 $(document).ready(function() {
+
+  // Graphs
+  const width = 0.75; // in vw
+  const height = 0.5; // in vh
+  let vw, vh;
+  if (window.innerWidth !== undefined && window.innerHeight !== undefined) {
+    vw = window.innerWidth;
+    vh = window.innerHeight;
+  } else {
+    vw = document.documentElement.clientWidth;
+    vh = document.documentElement.clientHeight;
+  }
+
+  const vwToPx = (sizeInVw) => sizeInVw * vw;
+  const vhToPx = (sizeInVh) => sizeInVh * vh;
+
+  const numTicksX = 9;
+  const numTicksY = 10;
+  const tickSizeVw = width / numTicksX;
+  const tickSizeVh = height / numTicksY;
+  const axisLengthXPx = vwToPx(width - tickSizeVw);
+  const axisLengthYPx = vhToPx(height - tickSizeVh);
+
+
   $('#fullpage').fullpage({
     anchors: ['intro', 'child-mortality', 'life-expectancy', 'extreme-poverty', 'world-gdp', 'literacy', 'challenges', 'you-have-the-power-to-help', 'do-it-today', 'footer'],
     sectionsColor: ['#F2F2F2', '#D6D6D6', '#F2F2F2', '#D6D6D6', '#F2F2F2', '#D6D6D6', '#F2F2F2', '#D6D6D6', '#F2F2F2', '#1B1C1C'],
     navigation: true,
     navigationPosition: 'right',
     navigationTooltips: ['Introduction', 'Child Mortality', 'Life Expectancy', 'Extreme Poverty', 'World\'s GDP', 'Literacy', 'Challenges', 'How you can help', 'Do it today'],
+    responsiveHeight: 666,
     afterLoad: function(anchorLink, index) {
       let section = '#section' + index;
       //todo: fix the selection below
@@ -19,11 +44,11 @@ $(document).ready(function() {
     onLeave: function(anchorLink, index) {
       let section = '#section' + index;
       let rect = $(section + ' > div > div.container > div.flip > div > svg > g > rect');
-      rect.attr('width', '732');
+      rect.attr('width', axisLengthXPx + 20);
     }
   });
 
-  $('.bg-image-zoom-in').css('transform', 'scale(1.2)');
+  $('.zoom-in').css('transform', 'scale(1.2)');
 
   //I was playing around with animations
   //the idea was that if you mouse over on left-hand shape all shapes move slowly to right
@@ -58,184 +83,185 @@ $(document).ready(function() {
           $('.polygonal-shape-right').css('left', '900px')
       })
    */
-});
-
-const width = 800;
-const height = 400;
-
-const drawGraph = (container, data, statLabel, yScale = [0, 100]) => {
-
-  const minYear = d3.min(data, d => d.year);
-  const maxYear = d3.max(data, d => d.year);
-
-  let x = d3.scaleLinear().domain([minYear, maxYear]).range([0, width - 70]);
-  let y = d3.scaleLinear().domain(yScale).range([height - 70, 0]);
-
-  let xAxis = d3.axisBottom(x)
-    .tickFormat(d3.format('d'));
-  let yAxis = d3.axisLeft(y);
-
-  let xAxisGrid = d3.axisBottom(x)
-    .ticks(9)
-    .tickSize(height - 70, 0)
-    .tickFormat('');
-
-  let yAxisGrid = d3.axisRight(y)
-    .ticks(10)
-    .tickSize(width - 70, 0)
-    .tickFormat('');
-
-  //it assumes the background color of the rectangle should be the background color of div.container
-  //which is two levels above the graph container div -- div.container > div.flip > container
-  let backgroundColor = d3.select(container)
-    .select(function() {
-      return this.parentNode;
-    })
-    .select(function() {
-      return this.parentNode;
-    })
-    .style('background-color');
-
-  let svg = d3.select(container)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
-
-  let chartGroup = svg.append('g')
-    .attr('transform', 'translate(50,50)');
-
-  let line = d3.line()
-    .x(d => x(d.year))
-    .y(d => y(d[statLabel]));
-
-  let area = d3.area()
-    .x(d => x(d.year))
-    .y0(height - 70)
-    .y1(d => y(d[statLabel]));
-
-  const defs = svg.append('defs');
-
-  const redGradient = defs.append('linearGradient')
-    .attr('id', 'redGradient')
-    .attr('x1', '0%')
-    .attr('y1', '0%')
-    .attr('x2', '100%')
-    .attr('y2', '100%')
-    .attr('spreadMethod', 'pad');
-  redGradient.append('stop')
-    .attr('offset', '0%')
-    .attr('stop-color', '#c00')
-    .attr('stop-opacity', 1);
-  redGradient.append('stop')
-    .attr('offset', '100%')
-    .attr('stop-color', backgroundColor)
-    .attr('stop-opacity', '0.2');
-
-  const greenGradient = defs.append('linearGradient')
-    .attr('id', 'greenGradient')
-    .attr('x1', '0%')
-    .attr('y1', '0%')
-    .attr('x2', '100%')
-    .attr('y2', '100%')
-    .attr('spreadMethod', 'pad');
-  greenGradient.append('stop')
-    .attr('offset', '0%')
-    .attr('stop-color', backgroundColor)
-    .attr('stop-opacity', '0.2');
-  greenGradient.append('stop')
-    .attr('offset', '100%')
-    .attr('stop-color', '#008000')
-    .attr('stop-opacity', 1);
-
-  let gradient;
-  if (data[0][statLabel] > data.slice(-1)[0][statLabel]) {
-    gradient = 'url(#redGradient)';
-  } else {
-    gradient = 'url(#greenGradient)';
-  }
-
-  var tooltip = d3.select('body')
-    .append('div')
-    .attr('class', 'graph-tooltip')
-    .text('a simple tooltip');
-
-  chartGroup.append('path')
-    .attr('d', area(data))
-    .style('fill', gradient)
-    .style('fill-opacity', '0.6')
-    .style('stroke-width', '0');
-
-  chartGroup.append('path')
-    .attr('d', line(data))
-    .attr('class', 'graph-line');
-
-  let graphDots = chartGroup.append('g')
-    .attr('class', 'graph-dot')
-    .attr('width', width)
-    .attr('height', height);
-
-  // hack for getting the group started at 0
-  graphDots.append('circle')
-    .attr('r', 1)
-    .attr('cx', 0)
-    .attr('cy', 0);
-
-  graphDots.selectAll('.circle').data(data)
-    .enter()
-    .append('circle')
-    .attr('r', 3)
-    // I can't make them animate, so I've made them invisible ....
-    .attr('fill-opacity', 1)
-    .style('fill', '#fff')
-    .attr('cx', d => x(d.year))
-    .attr('cy', d => y(d[statLabel]))
-    // & with tooltip
-    .on('mouseover', function(event) {
-      tooltip.style('visibility', 'visible');
-      tooltip.text(`${event.year}: ${event[statLabel]}`);
-    })
-    .on('mousemove', function() {
-      tooltip.style('top', (d3.event.pageY - 50) + 'px').style('left', (d3.event.pageX) + 'px');
-    })
-    .on('mouseout', function() {
-      return tooltip.style('visibility', 'hidden');
-    });
 
 
-  chartGroup.append('rect')
-    .attr('x', -1 * (width - 66))
-    .attr('y', -1 * (height - 70))
-    .attr('height', height - 70)
-    .attr('width', width - 66)
-    .attr('fill', backgroundColor)
-    .attr('transform', 'rotate(180)');
 
-  chartGroup.append('g').attr('transform', 'translate(0, 330)').call(xAxis);
-  chartGroup.append('g').call(yAxis);
 
-  chartGroup.append('g').call(yAxisGrid).attr('class', 'axis-grid');
-  chartGroup.append('g').call(xAxisGrid).attr('class', 'axis-grid');
+  const drawGraph = (container, data, statLabel, yScale = [0, 100]) => {
 
-};
+    const minYear = d3.min(data, d => d.year);
+    const maxYear = d3.max(data, d => d.year);
 
-d3.csv('/assets/datasets/child-mortality.csv').then(data => {
-  drawGraph('#child-mortality-graph-container', data, 'notSurvivingFiveYears');
-});
+    let x = d3.scaleLinear().domain([minYear, maxYear]).range([0, axisLengthXPx]);
+    let y = d3.scaleLinear().domain(yScale).range([axisLengthYPx, 0]);
 
-d3.csv('/assets/datasets/life-expectancy-in-uk.csv').then(data => {
-  drawGraph('#life-expectancy-graph-container', data, 'lifeExpectancyInUK');
-});
+    let xAxis = d3.axisBottom(x)
+      .tickFormat(d3.format('d'));
+    let yAxis = d3.axisLeft(y);
 
-d3.csv('/assets/datasets/extreme-poverty-percentage.csv').then(data => {
-  drawGraph('#extreme-poverty-graph-container', data, 'extremePovertyPercentage');
-});
+    let xAxisGrid = d3.axisBottom(x)
+      .ticks(numTicksX)
+      .tickSize(axisLengthYPx, 0)
+      .tickFormat('');
 
-d3.csv('/assets/datasets/gdp.csv').then(data => {
-  drawGraph('#gdp-graph-container', data, 'gdp', [1000, 15000]);
-});
+    let yAxisGrid = d3.axisRight(y)
+      .ticks(numTicksY)
+      .tickSize(axisLengthXPx, 0)
+      .tickFormat('');
 
-d3.csv('/assets/datasets/literate-illiterate.csv').then(data => {
-  drawGraph('#literacy-graph-container', data, 'literatePercentage');
+    //it assumes the background color of the rectangle should be the background color of div.container
+    //which is two levels above the graph container div -- div.container > div.flip > container
+    let backgroundColor = d3.select(container)
+      .select(function() {
+        return this.parentNode;
+      })
+      .select(function() {
+        return this.parentNode;
+      })
+      .style('background-color');
+
+    let svg = d3.select(container)
+      .append('svg')
+      .attr('width', vwToPx(width + tickSizeVw))
+      .attr('height', vhToPx(height + tickSizeVh));
+
+    let chartGroup = svg.append('g')
+      .attr('transform', `translate(${vwToPx(tickSizeVw)}, ${vhToPx(tickSizeVh)})`);
+
+    let line = d3.line()
+      .x(d => x(d.year))
+      .y(d => y(d[statLabel]));
+
+    let area = d3.area()
+      .x(d => x(d.year))
+      .y0(axisLengthYPx)
+      .y1(d => y(d[statLabel]));
+
+    const defs = svg.append('defs');
+
+    const redGradient = defs.append('linearGradient')
+      .attr('id', 'redGradient')
+      .attr('x1', '0%')
+      .attr('y1', '0%')
+      .attr('x2', '100%')
+      .attr('y2', '100%')
+      .attr('spreadMethod', 'pad');
+    redGradient.append('stop')
+      .attr('offset', '0%')
+      .attr('stop-color', '#c00')
+      .attr('stop-opacity', 1);
+    redGradient.append('stop')
+      .attr('offset', '100%')
+      .attr('stop-color', backgroundColor)
+      .attr('stop-opacity', '0.2');
+
+    const greenGradient = defs.append('linearGradient')
+      .attr('id', 'greenGradient')
+      .attr('x1', '0%')
+      .attr('y1', '0%')
+      .attr('x2', '100%')
+      .attr('y2', '100%')
+      .attr('spreadMethod', 'pad');
+    greenGradient.append('stop')
+      .attr('offset', '0%')
+      .attr('stop-color', backgroundColor)
+      .attr('stop-opacity', '0.2');
+    greenGradient.append('stop')
+      .attr('offset', '100%')
+      .attr('stop-color', '#008000')
+      .attr('stop-opacity', 1);
+
+    let gradient;
+    if (data[0][statLabel] > data.slice(-1)[0][statLabel]) {
+      gradient = 'url(#redGradient)';
+    } else {
+      gradient = 'url(#greenGradient)';
+    }
+
+    let tooltip = d3.select('body')
+      .append('div')
+      .attr('class', 'graph-tooltip')
+      .text('a simple tooltip');
+
+    chartGroup.append('path')
+      .attr('d', area(data))
+      .style('fill', gradient)
+      .style('fill-opacity', '0.6')
+      .style('stroke-width', '0');
+
+    chartGroup.append('path')
+      .attr('d', line(data))
+      .attr('class', 'graph-line');
+
+    let graphDots = chartGroup.append('g')
+      .attr('class', 'graph-dot')
+      .attr('width', vwToPx(width))
+      .attr('height', vhToPx(height));
+
+    // hack for getting the group started at 0
+    graphDots.append('circle')
+      .attr('r', 1)
+      .attr('cx', 0)
+      .attr('cy', 0);
+
+    graphDots.selectAll('.circle').data(data)
+      .enter()
+      .append('circle')
+      .attr('r', 3)
+      // I can't make them animate, so I've made them invisible ....
+      .attr('fill-opacity', 1)
+      .style('fill', '#fff')
+      .attr('cx', d => x(d.year))
+      .attr('cy', d => y(d[statLabel]))
+      // & with tooltip
+      .on('mouseover', function(event) {
+        tooltip.style('visibility', 'visible');
+        tooltip.text(`${event.year}: ${event[statLabel]}`);
+      })
+      .on('mousemove', function() {
+        tooltip.style('top', (d3.event.pageY - 50) + 'px').style('left', (d3.event.pageX) + 'px');
+      })
+      .on('mouseout', function() {
+        return tooltip.style('visibility', 'hidden');
+      });
+
+
+    chartGroup.append('rect')
+      .attr('x', -1 * (axisLengthXPx + 20))
+      .attr('y', -1 * axisLengthYPx)
+      .attr('height', axisLengthYPx) // Fix something here so that the last dot is properly hidden
+      .attr('width', axisLengthXPx + 20)
+      .attr('fill', backgroundColor)
+      .attr('transform', 'rotate(180)');
+
+    chartGroup.append('g').attr('transform', `translate(0, ${vhToPx(tickSizeVh * (numTicksY - 1))})`).call(xAxis);
+    chartGroup.append('g').call(yAxis);
+
+    chartGroup.append('g').call(yAxisGrid).attr('class', 'axis-grid');
+    chartGroup.append('g').call(xAxisGrid).attr('class', 'axis-grid');
+
+  };
+
+  d3.csv('/assets/datasets/child-mortality.csv').then(data => {
+    drawGraph('#child-mortality-graph-container', data, 'notSurvivingFiveYears');
+  });
+
+  d3.csv('/assets/datasets/life-expectancy-in-uk.csv').then(data => {
+    drawGraph('#life-expectancy-graph-container', data, 'lifeExpectancyInUK');
+  });
+
+  d3.csv('/assets/datasets/extreme-poverty-percentage.csv').then(data => {
+    drawGraph('#extreme-poverty-graph-container', data, 'extremePovertyPercentage');
+  });
+
+  d3.csv('/assets/datasets/gdp.csv').then(data => {
+    drawGraph('#gdp-graph-container', data, 'gdp', [1000, 15000]);
+  });
+
+  d3.csv('/assets/datasets/literate-illiterate.csv').then(data => {
+    drawGraph('#literacy-graph-container', data, 'literatePercentage');
+  });
+
 });
 
 
