dd5695a5110d7aa4cc2caffd1fa11b35f68654fe
Lars Mennen <mennen.lars@gmail.com>
2020-11-29 16:36:05 +0100

    Remove reference to image

--- website/custom/index/assets/js/progress.js
+++ website/custom/index/assets/js/progress.js
@@ -286,11 +286,6 @@ particlesJS('particles-js', {
       'polygon': {
         'nb_sides': 5
       },
-      'image': {
-        'src': 'img/github.svg',
-        'width': 100,
-        'height': 100
-      }
     },
     'opacity': {
       'value': 0.5,
