aac724862fa04fef3caed4d5853c8e83dbc3b535
Lars Mennen <mennen.lars@gmail.com>
2020-12-04 14:21:48 +0100

    Add slde on gender equality

--- website/custom/index/assets/js/progress.js
+++ website/custom/index/assets/js/progress.js
@@ -8,11 +8,11 @@ import './jquery.svganim.min'
 $(document).ready(function() {
 
   $('#fullpage').fullpage({
-    anchors: ['intro', 'child-mortality', 'life-expectancy', 'extreme-poverty', 'world-gdp', 'literacy', 'challenges', 'you-have-the-power-to-help', 'do-it-today', 'footer'],
-    sectionsColor: ['#F2F2F2', '#D6D6D6', '#F2F2F2', '#D6D6D6', '#F2F2F2', '#D6D6D6', '#F2F2F2', '#D6D6D6', '#F2F2F2', '#1B1C1C'],
+    anchors: ['intro', 'child-mortality', 'life-expectancy', 'extreme-poverty', 'world-gdp', 'literacy', 'gender-equality', 'challenges', 'you-have-the-power-to-help', 'do-it-today', 'footer'],
+    sectionsColor: ['#F2F2F2', '#D6D6D6', '#F2F2F2', '#D6D6D6', '#F2F2F2', '#D6D6D6', '#F2F2F2', '#D6D6D6', '#F2F2F2', '#D6D6D6', '#1B1C1C'],
     navigation: true,
     navigationPosition: 'right',
-    navigationTooltips: ['Introduction', 'Child Mortality', 'Life Expectancy', 'Extreme Poverty', 'World\'s GDP', 'Literacy', 'Challenges', 'How you can help', 'Do it today'],
+    navigationTooltips: ['Introduction', 'Child Mortality', 'Life Expectancy', 'Extreme Poverty', 'World\'s GDP', 'Literacy', 'Gender Equalty', 'Challenges', 'How you can help', 'Do it today'],
     responsiveHeight: 666,
     afterLoad: function(anchorLink, index) {
       let section = '#section' + index;
@@ -26,11 +26,11 @@ $(document).ready(function() {
       let section = '#section' + toIndex;
       let rect = $(section + ' > div > div.container > div.flip > div > svg > g > rect');
       rect.attr('width', window.axisLengthXPx + 20);
-      if (fromIndex === 9 && toIndex === 10) {
-        // Scrolling to footer should not hide content on do-it-today (9)
-        $('#section9 .fade-up').addClass('active');
-      } else if (fromIndex === 10 && toIndex === 9) {
-        $('#section9 .fade-up.active').removeClass('active');
+      if (fromIndex === 10 && toIndex === 11) {
+        // Scrolling to footer should not hide content on do-it-today (10)
+        $('#section10 .fade-up').addClass('active');
+      } else if (fromIndex === 11 && toIndex === 10) {
+        $('#section10 .fade-up.active').removeClass('active');
       }
     }
   });
@@ -226,6 +226,7 @@ $(document).ready(function() {
     "poverty": d3.csv('/assets/datasets/extreme-poverty-percentage.csv'),
     "gdp": d3.csv('/assets/datasets/gdp.csv'),
     "literacy": d3.csv('/assets/datasets/literate-illiterate.csv'),
+    "gender-equality": d3.csv('/assets/datasets/gender-equality.csv'),
   }
 
   const drawAllGraphs = () => {
@@ -247,6 +248,10 @@ $(document).ready(function() {
 
     dataPromises["literacy"].then(data => {
       drawGraph('#literacy-graph-container', data, 'literatePercentage');
+    });
+
+    dataPromises["gender-equality"].then(data => {
+      drawGraph('#gender-equality-graph-container', data, 'medianGenderPayGap');
     });
   };
 
