8e60764d3de2ce4e6d2195d15b3f29ffbe1013b7
Mariusz <ulejczykm@gmail.com>
2021-02-28 18:56:56 +0100

    add return with removeEventListener to useEffect

--- website/components/NewsletterExitIntent.js
+++ website/components/NewsletterExitIntent.js
@@ -3,45 +3,49 @@ import React, { useState, useEffect } from "react"
 import Modal from "react-modal"
 
 const customModalStyles = {
-    overlay: {
-      backgroundColor: "rgba(0, 0, 0, 0.75)"
-    },
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-30%",
-      transform: "translate(-50%, -50%)",
-      paddingLeft: "20px"
-    }
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.75)"
+  },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-30%",
+    transform: "translate(-50%, -50%)",
+    paddingLeft: "20px"
   }
+};
 
 Modal.setAppElement('body');
 
 const NewsletterExitIntent = () => {
-    const [isMouseOut, setMouseOut] = useState(false);
+  const [isMouseOut, setMouseOut] = useState(false);
 
-    const mouseOut = () => {
-            setMouseOut(true);
-            document.removeEventListener('mouseleave', mouseOut);
-    };
+  const mouseOut = () => {
+    setMouseOut(true);
+    document.removeEventListener('mouseleave', mouseOut);
+  };
 
-    useEffect(() => {
-        document.addEventListener('mouseleave', mouseOut); 
-    }, [setMouseOut]);
+  useEffect(() => {
+    document.addEventListener('mouseleave', mouseOut);
+    
+    return (() => {
+      document.removeEventListener('mouseleave', mouseOut);
+    });
+  }, [setMouseOut]);
 
-    return (
-        <Modal
-            isOpen={isMouseOut}
-            style={customModalStyles}
-            >
-                <p>
-                    Newsletter
-                </p>
-                <button  onClick={() => setMouseOut(false)}>close</button>
-        </Modal>
-    )
+  return (
+    <Modal
+      isOpen={isMouseOut}
+      style={customModalStyles}
+    >
+      <p>
+          Newsletter
+      </p>
+      <button  onClick={() => setMouseOut(false)}>close</button>
+    </Modal>
+  )
 };
 
 export default NewsletterExitIntent;
