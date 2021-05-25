3799c1318f751f52796c02ce2d67b8e860c3507f
Mariusz Ulejczyk <ulejczykm@gmail.com>
2021-05-25 19:49:29 +0200

    fixup! add Header translations

--- extension/src/panel/components/Header.tsx
+++ extension/src/panel/components/Header.tsx
@@ -19,14 +19,14 @@ export const Header: React.FC<Props> = (props: Props) => {
       <div className="header__login">
         {props.user ? (
           <div>
-            <span className="text-emphasized">{browser.i18n.getMessage('headerLoggedInAs')}</span>
+            <span className="text-emphasized">{browser.i18n.getMessage('loggedInAs')}</span>
             <br />
             <span className="text-muted">{props.user.email}</span>
           </div>
         ) : (
           <div>
             <button className="button-link uppercase-link" onClick={props.onLoginOrRegisterClick}>
-              {browser.i18n.getMessage('headerButtonLoginOrRegister')}
+              {browser.i18n.getMessage('loginOrRegister')}
             </button>
           </div>
         )}
