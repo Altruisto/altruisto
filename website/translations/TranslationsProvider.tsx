d5cbb0d58b4c87bcfa549963c37a8c29c2822418
Mariusz Ulejczyk <ulejczykm@gmail.com>
2021-06-04 04:42:07 +0200

    language detection for the app

--- website/translations/TranslationsProvider.tsx
+++ website/translations/TranslationsProvider.tsx
@@ -1,12 +1,28 @@
-import React, { FunctionComponent } from "react"
+import React, { FunctionComponent, useEffect, useState } from "react"
 import { IntlProvider } from "react-intl"
 import { en } from "./strings/en"
+import { pl } from "./strings/pl"
+
+const getMessages = (language: string) => {
+  // because Safari uses notation pl-pl, I used regex test instead of checking hardcoded string
+  if (/^en\b/.test(language)) {
+    return en;
+  } else if (/^pl\b/.test(language)) {
+    return pl;
+  }
+  return en;
+}
 
 const TranslationsProvider: FunctionComponent<any> = ({ children }) => {
   // logic for storing language preference in localStorage or recognizing browser's language ()
 
+  const language = navigator.language;
+
   return (
-    <IntlProvider locale={"en"} messages={en}>
+    <IntlProvider 
+      locale={language || 'en'} 
+      messages={getMessages(language)}
+    >
       {children}
     </IntlProvider>
   )
