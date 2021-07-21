b0748f40c7e2e95f6a7e8382478ab114d348e96b
Daniel Wyrzykowski <kovsky0@users.noreply.github.com>
2021-07-21 14:07:51 +0200

    feat: add ebay restrictions (#118)

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,9 +1,10 @@
 import { onInstalled } from "./background/on-installed"
 import { onAlarm } from "./background/on-alarm"
 import { monitorActivatedPartners } from "./background/monitor-activated-partners"
-import { recognizeOtherAffiliates } from "./background/recognize-other-affiliates"
+import { recognizeOtherAffiliates, recognizeEbayAffiliates } from "./background/recognize-other-affiliates"
 
 onInstalled()
 onAlarm()
 monitorActivatedPartners()
 recognizeOtherAffiliates()
+recognizeEbayAffiliates()
