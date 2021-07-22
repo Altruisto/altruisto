0cd5efac095d8fe81931a184a0de37a7a314974d
Daniel Wyrzykowski <kovsky0@users.noreply.github.com>
2021-07-22 16:03:49 +0200

    feat: join the team page (#119)

---
+++ website/pages/join-the-team.tsx
@@ -0,0 +1,196 @@
+import { MinimalLayout } from "../components/layouts/MinimalLayout"
+
+const JoinTheTeam = () => (
+  <MinimalLayout>
+    <h2>About us</h2>
+    <p>
+      We have developed{" "}
+      <a style={{ textDecoration: "underline" }} target="_blank" href="http://altruisto.com">
+        altruisto.com
+      </a>{" "}
+      - an app that lets users donate money to charities while shopping online. At no extra cost. We
+      have over 1000 partner shops including eBay, AliExpress, Booking.com, Etsy, Microsoft. The
+      donation is usually between 2% and 6% of the price of the product.
+    </p>
+
+    <p>
+      We&#x27;re a social enterprise and this is a not-for-profit project - all the profits are
+      donated to charities. We&#x27;re self-funded and still in an early stage so you have a chance
+      to play a huge part in building our product and company.
+    </p>
+
+    <p>
+      In 2019 global e-commerce amounted to
+      <a
+        style={{ textDecoration: "underline" }}
+        target="_blank"
+        href="https://www.statista.com/statistics/379046/worldwide-retail-e-commerce-sales/"
+      >
+        3.5 trillion dollars
+      </a>
+      . If we could move just 1% of that towards the most pressing issues we could protect everyone
+      at risk from malaria and treat 2 billion people from parasites. In the first year.
+    </p>
+
+    <p> Our goal is to make that happen.</p>
+
+    <h2>About the position</h2>
+    <p>
+      We are looking for someone to <strong>help us grow</strong>. We are constantly testing new
+      ways to bring more users on board. We need someone who can facilitate our experiments and
+      <strong>execute</strong> our, meaning yours as well, ideas.
+    </p>
+
+    <p id="98f2e342-2eee-45e2-92ca-c1a77b0f425b">
+      Following are a few exemplary projects you might work on:
+    </p>
+    <ul id="4b48f6fc-1b3e-4ab6-893e-2538886dbdbd">
+      <li>Reaching out to micro-influencers in our space to collaborate</li>
+    </ul>
+    <ul id="4dc36a29-127d-4ded-b4a3-c4dbc10345ef">
+      <li>
+        Combining a list of freelance translators to create local language versions of our app
+      </li>
+    </ul>
+    <ul id="553980e3-db0f-463c-aaff-2d11f92d312d">
+      <li>Reaching out to potential partners and explaining the idea behind the app</li>
+    </ul>
+    <ul id="1fb67dc8-c624-452b-95a7-751fde176ed6">
+      <li>
+        Creating drafts and copy for landing page for specific purposes (eg. Black Friday,
+        Single&#x27;s Day, etc.)
+      </li>
+    </ul>
+
+    <p>
+      This is a <strong>remote position</strong>. You&#x27;re free to choose to work where you work
+      best: home, co-working space, coffee shops.
+    </p>
+
+    <p>
+      We work on a <strong>flexible schedule</strong>. We meet at a short daily meeting at around
+      12:45 pm CET, other than that, you&#x27;re free to do your job at whatever time is best for
+      you. You’ll have the freedom and autonomy to do your best work and plenty of support along the
+      way. However, we do value calm, concentrated effort rather than multiple short bursts of time
+      scattered throughout the day.
+    </p>
+
+    <p>
+      We&#x27;re open to various modes of cooperation, be it
+      <strong>per project, part-time, or full-time.</strong>
+    </p>
+
+    <p>
+      <strong>Forms of employment:</strong>
+    </p>
+    <ul id="d4b4f433-ccf5-444b-95fb-1654bfc260ed">
+      <li>B2B</li>
+    </ul>
+    <ul id="b0ba0ac2-adf4-4a5d-95eb-2f5e4549dea9">
+      <li>Contract of mandate (umowa zlecenie)</li>
+    </ul>
+
+    <h2>About you</h2>
+    <p> We are looking for people who:</p>
+    <ul id="1ff84453-7f40-49aa-89d9-a5ac7d279b35">
+      <li>
+        are <strong>self-sufficient</strong>.
+        <a
+          href="https://signalvnoise.com/posts/1430-hire-managers-of-one"
+          style={{ textDecoration: "underline" }}
+          target="_blank"
+        >
+          Managers of one
+        </a>
+        . You can set your own objectives that are aligned with the goals of the project and you can
+        execute on these objectives. It doesn&#x27;t mean that you won&#x27;t get support when
+        needed but you can and like to work on your own. Extra points if you have read
+        <a
+          href="https://www.goodreads.com/book/show/25744928-deep-work"
+          style={{ textDecoration: "underline" }}
+          target="_blank"
+        >
+          Deep Work by Cal Newport
+        </a>
+        .
+      </li>
+    </ul>
+    <ul id="1849a1d0-071e-4424-9596-f8315652806c">
+      <li>
+        are <strong>proactive</strong>. You&#x27;re the type of person who after finishing a
+        project, looks for the next most important thing to do, instead of waiting to be told what
+        to do or watching Netflix.
+      </li>
+    </ul>
+    <ul id="4e5e9cca-2075-445c-b46c-fd6884bad7b5">
+      <li>
+        are <strong>eager to learn. </strong>You thrive in a changing environment and like to
+        explore new areas rather than just execute pre-defined procedures.
+      </li>
+    </ul>
+    <ul id="b2079ed2-56bd-427e-9550-123b1fdcb736">
+      <li>
+        most importantly, <strong>care for others</strong>. We are working on a social project and
+        we are looking for people who have the same level of passion for helping fellow humans and
+        animals as we do.
+      </li>
+    </ul>
+
+    <h2>Requirements</h2>
+    <ul id="fd843b13-8eed-4ab9-b678-97bb6a0ad728">
+      <li>
+        Your written English is perfect and you actually
+        <strong>enjoy writing.</strong> A huge part of the job is writing - emails, tweets,
+        copywriting for web pages, emails, and more emails. Also, have we mentioned emails?
+      </li>
+    </ul>
+    <ul id="eb28b534-9b52-4217-a16a-f406b3cb78e7">
+      <li>
+        You have <strong>an eye for good design</strong> and know how to use tools like Canva.
+      </li>
+    </ul>
+    <ul id="9eca25a3-818d-4bd6-b5bb-9b20b08893b4">
+      <li>
+        You can pick up how to use new tools and you&#x27;re not afraid to experiment with them.
+      </li>
+    </ul>
+    <p>
+      We don&#x27;t care about your formal education or if you had previous experience in marketing
+      or not.
+    </p>
+    <p>
+      We care about what type of person and you are and whether you can learn needed skills on the
+      go. If you&#x27;re thoughtful, kind, conscientious.
+    </p>
+    <h2>Recruitment process</h2>
+    <p>
+      Please send us your resume to hello@altruisto.com and fill out{" "}
+      <a
+        href="https://forms.gle/EfASBJkVAkFV3uDs6"
+        style={{ textDecoration: "underline" }}
+        target="_blank"
+      >
+        this form
+      </a>
+      .
+    </p>
+    <p id="a9e39499-d714-482e-b81d-1c9285611168">
+      If we think we&#x27;re a match we&#x27;ll invite you to an interview. The interview takes
+      60-90 minutes and is conducted via Google Meet. We&#x27;ll talk about your past and our
+      future. We&#x27;ll ask questions about your past work and you&#x27;ll ask questions about
+      whatever you want.
+    </p>
+    <p id="e2dc1204-5e47-4791-8df2-95f5532bee87">
+      The last part is a small exemplary project that should take up to 2 hours (but probably much
+      less than that). We do realise this is a big commitment so there is no deadline for turning in
+      your results. The project will be similar to something we actually work on.
+    </p>
+    <p>
+      Please note that we&#x27;re unable to provide individual feedback about your performance in
+      the recruitment process. We&#x27;re a small team and we simply don&#x27;t have the capacity to
+      offer personalised feedback.
+    </p>
+  </MinimalLayout>
+)
+
+export default JoinTheTeam
