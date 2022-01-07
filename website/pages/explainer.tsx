b34f0c2566db7de532838557763b22f3925db97e
Daniel Wyrzykowski <kovsky0@users.noreply.github.com>
2022-01-07 15:40:55 +0100

    feat: explainer page (#130)

---
+++ website/pages/explainer.tsx
@@ -0,0 +1,95 @@
+import { MinimalLayout } from "../components/layouts/MinimalLayout"
+
+const TermsOfService = () => (
+  <MinimalLayout>
+    <h1>References and explanation of data used</h1>
+    <h2>Sources</h2>
+    <h3 style={{ marginTop: 20 }}>Malaria deaths</h3>
+    <p>
+      In 2019, there were 229 million malaria cases globally that led to 409,000 deaths. Of these
+      deaths, 67 per cent (274,000) were children under 5 years of age. This translates into a daily
+      toll of nearly 750 children under age 5.
+    </p>
+    <p></p>
+    <code>750 / 416 max passengers of Boeing 747 ~= 2</code>
+    <p>
+      <a href="https://data.unicef.org/topic/child-health/malaria/">
+        https://data.unicef.org/topic/child-health/malaria/
+      </a>
+      <br />
+      <a href="https://www.cs.mcgill.ca/~rwest/wikispeedia/wpcd/wp/b/Boeing_747.htm">
+        https://www.cs.mcgill.ca/~rwest/wikispeedia/wpcd/wp/b/Boeing_747.htm
+      </a>
+    </p>
+    <h3 style={{ marginTop: 20 }}>Vetted charities</h3>
+    <p>
+      We follow recommendations of independent meta-charities and evaluators of charities'
+      effectivenes, such as GiveWell, Animal Charity Evaluators, The Life You Can Save, Center for
+      Effective Altruism.
+    </p>
+    <h3 style={{ marginTop: 20 }}>Effectiveness of LLINs in prevention of malaria</h3>
+    <p>
+      Insecticide-treated bed nets prevent deaths and many other non-fatal cases of malaria, and are
+      relatively inexpensive: about $4.85 per net and one net on average protects two people.
+    </p>
+    <p>
+      <a href="https://www.givewell.org/charities/amf">https://www.givewell.org/charities/amf</a>
+    </p>
+    <h3 style={{ marginTop: 20 }}>Potential revenue</h3>
+    <p>In 2019 retail e-commerce sales worldwide amounted to 3.53 trillion USD</p>
+    <a href="https://www.statista.com/statistics/379046/worldwide-retail-e-commerce-sales/">
+      https://www.statista.com/statistics/379046/worldwide-retail-e-commerce-sales/
+    </a>
+    <h3 style={{ marginTop: 20 }}>Costs for solving different problems</h3>
+    <strong>ELIMINATING EXTREME POVERTY</strong>
+    <p>$175 billion each year to solve over 2 decades (Jeffery Sachs)</p>
+    <p>
+      <a
+        href="http://www.visionofearth.org/economics/ending-poverty/how-much-would-it-cost-to-end-extreme-poverty-in-the-world/
+"
+      >
+        http://www.visionofearth.org/economics/ending-poverty/how-much-would-it-cost-to-end-extreme-poverty-in-the-world/
+      </a>
+    </p>
+    <strong>RAINFOREST</strong>
+    <p>$1-$100 to save acre of rainforest Cool Earth</p>
+    <p>
+      2.4 billion square miles = 1.5 billion acres of tropical rainforest Upper estimate $150
+      billion to buy up all rainforest
+    </p>
+    <p>
+      <a href="http://www.ted.com/conversations/22897/saving_rainforest_is_hugely_co.html">
+        http://www.ted.com/conversations/22897/saving_rainforest_is_hugely_co.html
+      </a>
+      <br />
+      <a href="https://www.coolearth.org/">https://www.coolearth.org/</a>
+      <br />
+      <a href="http://rainforests.mongabay.com/0101.htm">
+        http://rainforests.mongabay.com/0101.htm
+      </a>
+      <br />
+    </p>
+    <strong>MARS MISSIONS</strong>
+    <p>
+      <a href="http://news.nationalgeographic.com/news/2014/04/140422-mars-mission-manned-cost-science-space/">
+        http://news.nationalgeographic.com/news/2014/04/140422-mars-mission-manned-cost-science-space/
+      </a>
+    </p>
+    <p>
+      The examples used were inspired by{" "}
+      <a href="https://www.youtube.com/watch?v=LtWINl3C_7s" target="_blank">
+        Effective Altruism | Beth Barnes | TEDxExeter
+      </a>{" "}
+      and{" "}
+      <a href="https://slatestarcodex.com/2014/12/19/nobody-is-perfect-everything-is-commensurable/">
+        NOBODY IS PERFECT, EVERYTHING IS COMMENSURABLE
+      </a>
+      . See more details here:{" "}
+      <a href="https://docs.google.com/document/d/1iIdA7PnjSVQAv6E75QWocmCeq62lU2YUwOz1pVlY42Q/edit#heading=h.gjdgxs">
+        https://docs.google.com/document/d/1iIdA7PnjSVQAv6E75QWocmCeq62lU2YUwOz1pVlY42Q/edit#heading=h.gjdgxs
+      </a>
+    </p>
+  </MinimalLayout>
+)
+
+export default TermsOfService
