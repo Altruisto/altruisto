3cb68aa93497b5bb104c47417bab625dee7df0b2
Daniel Wyrzykowski <kovsky0@users.noreply.github.com>
2022-02-28 10:34:06 +0100

    hacky env

--- website/utils/api-url.ts
+++ website/utils/api-url.ts
@@ -9,7 +9,7 @@ export const api = axios.create({
 })
 export const api2Url =
   process.env.NODE_ENV === "production"
-    ? "https://api2.altruisto.com/v2"
+    ? "https://altruisto-api-playground.herokuapp.com/v2"
     : "https://altruisto-api-playground.herokuapp.com/v2"
 export const api2 = axios.create({
   baseURL: api2Url
@@ -17,5 +17,5 @@ export const api2 = axios.create({
 
 export const getStripeApiKey = () =>
   process.env.NODE_ENV === "production"
-    ? "pk_live_51KWgX8ATvvx9S1LEKpR6T42uxh2gSFAoriu1GHPQq0nntuzLfVyubQMn2qndQxulU5A9KwdhfmOnE9IIJAv6zAdY00gFpGUouh"
+    ? "pk_test_51KWgX8ATvvx9S1LEtXxgh0aWJFEbYI2Ab2L0pGrAiwUKGWqT0qDwn5QOB64Rb684fh3dMKtZG1gg2M8zpVvmmShm00Iz9frXnN"
     : "pk_test_51KWgX8ATvvx9S1LEtXxgh0aWJFEbYI2Ab2L0pGrAiwUKGWqT0qDwn5QOB64Rb684fh3dMKtZG1gg2M8zpVvmmShm00Iz9frXnN"
