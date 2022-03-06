fa445c85bc663099a2b3e6446c94fd12d9a897c8
Daniel Wyrzykowski <kovsky0@users.noreply.github.com>
2022-03-06 16:07:04 +0100

    build: use envs

--- website/utils/api-url.ts
+++ website/utils/api-url.ts
@@ -7,15 +7,9 @@ export const apiUrl =
 export const api = axios.create({
   baseURL: apiUrl
 })
-// export const api2Url =
-//   process.env.NODE_ENV === "production"
-//     ? "https://altruisto-api-playground.herokuapp.com/v2"
-//     : "https://altruisto-api-playground.herokuapp.com/v2"
+
 export const api2 = axios.create({
-  baseURL: "http://localhost:9091/v2"
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL_2}/v2`
 })
 
-export const getStripeApiKey = () =>
-  process.env.NODE_ENV === "production"
-    ? "pk_test_51KWgX8ATvvx9S1LEtXxgh0aWJFEbYI2Ab2L0pGrAiwUKGWqT0qDwn5QOB64Rb684fh3dMKtZG1gg2M8zpVvmmShm00Iz9frXnN"
-    : "pk_test_51KWgX8ATvvx9S1LEtXxgh0aWJFEbYI2Ab2L0pGrAiwUKGWqT0qDwn5QOB64Rb684fh3dMKtZG1gg2M8zpVvmmShm00Iz9frXnN"
+export const getStripeApiKey = () => process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY
